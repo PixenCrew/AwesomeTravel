6f954cb4407d23d771808dd6aad4fb88dfc53547
hhhhea <haechang977@gmail.com>
2025-03-15 12:24:30 +0900

    kkjj-14 <test>(<Air>) : 오픈api통한 데이터 확인
    공공데이터 포탈에서 값이 제대로 들어오는지 확인하는 테스트이다.
     MemberRepository는 경로오타 수정

--- src/main/java/renewal/awesome_travel/Member/repository/MemberRepository.java
+++ src/main/java/renewal/awesome_travel/Member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package renewal.awesome_travel.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import renewal.awesome_travel.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     
