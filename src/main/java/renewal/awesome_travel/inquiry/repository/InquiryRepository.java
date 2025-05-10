eae94b0283ac9ceeb538e10aa45e63538d1dc4ff
hhhhea <haechang977@gmail.com>
2025-05-10 23:59:27 +0900

    KKJJ-21 <feature> (<Inquiry>) : 1대1문의 연관관계 변경
    1대1문의와 user간의 연관관계 변경

--- src/main/java/renewal/awesome_travel/inquiry/repository/InquiryRepository.java
+++ src/main/java/renewal/awesome_travel/inquiry/repository/InquiryRepository.java
@@ -7,9 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import renewal.awesome_travel.inquiry.entity.Inquiry;
+<<<<<<< Updated upstream
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     Page<Inquiry> findByUserId(Long userId, Pageable pageable);
+=======
+import renewal.awesome_travel.member.entity.User;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+    Page<Inquiry> findByUser(User user, Pageable pageable);
+
+>>>>>>> Stashed changes
 
     // 어드민 검색용: 키워드(title, content) 검색 + 답변 여부
     @Query("SELECT i FROM Inquiry i WHERE " +
@@ -21,6 +29,7 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
     // 사용자 본인 검색용: 본인 + 키워드(title, content) 검색 + 답변 여부
     @Query("SELECT i FROM Inquiry i WHERE " +
+<<<<<<< Updated upstream
             "i.userId = :userId AND " +
             "( :keyword IS NULL OR i.title LIKE %:keyword% OR i.content LIKE %:keyword% ) AND " +
             "( :isAnswered IS NULL OR i.isAnswered = :isAnswered )")
@@ -28,4 +37,14 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
                              @Param("keyword") String keyword,
                              @Param("isAnswered") Boolean isAnswered,
                              Pageable pageable);
+=======
+            "i.user = :user AND " +
+            "( :keyword IS NULL OR i.title LIKE %:keyword% OR i.content LIKE %:keyword% ) AND " +
+            "( :isAnswered IS NULL OR i.isAnswered = :isAnswered )")
+    Page<Inquiry> searchUser(@Param("user") User user,
+                             @Param("keyword") String keyword,
+                             @Param("isAnswered") Boolean isAnswered,
+                             Pageable pageable);
+
+>>>>>>> Stashed changes
 }
