f4c669f5e68528067126e820d80d1871f1bfbaef
JinK001 <kyj9447@gmail.com>
2025-08-31 20:29:24 +0900

    minor changes

--- src/main/java/renewal/awesome_travel/air/domain/RoundTripSearchResult.java
+++ src/main/java/renewal/awesome_travel/air/domain/RoundTripSearchResult.java
@@ -2,26 +2,26 @@ package renewal.awesome_travel.air.domain;
 
 
 import lombok.Getter;
-import renewal.awesome_travel.air.entity.Air;
+import renewal.awesome_travel.air.entity.SeatClass;
 import renewal.awesome_travel.air.utiles.SeatClassType;
 
 @Getter
 public class RoundTripSearchResult {
-    private Air outbound;
-    private Air inbound;
+    private SeatClass outbound;
+    private SeatClass inbound;
     private int totalPrice;
 
-    public RoundTripSearchResult(Air outbound, Air inbound, SeatClassType classType) {
+    public RoundTripSearchResult(SeatClass outbound, SeatClass inbound, SeatClassType classType) {
         this.outbound = outbound;
         this.inbound = inbound;
-        this.totalPrice = outbound.getPrice(classType) + inbound.getPrice(classType);
+        this.totalPrice = outbound.getPrice() + inbound.getPrice();
     }
 
-    public Air getOutbound() { return outbound; }
-    public Air getInbound() { return inbound; }
+    public SeatClass getOutbound() { return outbound; }
+    public SeatClass getInbound() { return inbound; }
 
-    public String getOutboundDepartTime() { return outbound.getDepart_time(); }
-    public String getInboundDepartTime() { return inbound.getDepart_time(); }
+    public String getOutboundDepartTime() { return outbound.getAir().getDepart_time(); }
+    public String getInboundDepartTime() { return inbound.getAir().getDepart_time(); }
 
     public int getTotalPrice() { return totalPrice; }
 }
