7609eb29eaa493f9cbe894564120cb702ac9f232
JinK001 <kyj9447@gmail.com>
2025-09-11 12:22:00 +0900

    kkjj-30 <refactor> 공용 엔티티, 리포지토리 사용 설정

--- src/main/java/renewal/awesome_travel/AwesomeTravelApplication.java
+++ src/main/java/renewal/awesome_travel/AwesomeTravelApplication.java
@@ -2,10 +2,22 @@ package renewal.awesome_travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+
+@EntityScan(basePackages = {
+		"renewal.common",
+		"renewal.awesome_travel"
+})
+@EnableJpaRepositories(basePackages = {
+		"renewal.common",
+		"renewal.awesome_travel"
+})
+
 public class AwesomeTravelApplication {
 
 	public static void main(String[] args) {
