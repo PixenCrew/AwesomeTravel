494d77f8690bc39ccca3e4245219c251d6e08bc2
JinK001 <kyj9447@gmail.com>
2025-11-11 01:48:14 +0000

    minor changes

--- src/main/java/renewal/awesome_travel/product/dto/ProductSpecification.java
+++ src/main/java/renewal/awesome_travel/product/dto/ProductSpecification.java
@@ -1,8 +1,10 @@
 package renewal.awesome_travel.product.dto;
 
 import org.springframework.data.jpa.domain.Specification;
-import jakarta.persistence.criteria.*;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 import renewal.common.entity.Product;
 import renewal.common.entity.Tour;
 
@@ -10,8 +12,9 @@ public class ProductSpecification {
 
     public static Specification<Product> keywordContains(String keyword) {
         return (root, query, builder) -> {
-            query.distinct(true); // 중복 제거
-
+            if (query != null) {
+                query.distinct(true); // 중복 제거
+            }
             // Product.keywords
             Join<Product, String> productKeywords = root.join("keywords", JoinType.LEFT);
             Predicate productPredicate = builder.like(productKeywords, "%" + keyword + "%");
