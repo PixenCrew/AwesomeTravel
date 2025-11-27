e6bbc842777060f06cba7644e24fa0334bd2eba4
hhhhea <haechang977@gmail.com>
2025-11-28 05:15:07 +0900

    KKJJ-54 <feat> (<review>) : 리뷰기능 추가

--- src/main/java/renewal/awesome_travel/review/repository/ReviewRepository.java
+++ src/main/java/renewal/awesome_travel/review/repository/ReviewRepository.java
@@ -1,8 +1,12 @@
 package renewal.awesome_travel.review.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import renewal.common.entity.Review;
 
@@ -11,5 +15,14 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByProductId(Long productId, Pageable pageable);
 
     Page<Review> findByWriterId(Long userId, Pageable pageable);
+
+    @Query("SELECT r.rating AS rating, COUNT(r) AS count FROM Review r WHERE r.product.id = :productId GROUP BY r.rating")
+    List<RatingCount> countByProductIdGroupByRating(@Param("productId") Long productId);
+
+    interface RatingCount {
+        Integer getRating();
+
+        Long getCount();
+    }
 
 }
