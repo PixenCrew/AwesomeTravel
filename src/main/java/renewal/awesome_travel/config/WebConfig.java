b373e55b39a370e0f1a76b42725d54b3df66f8ad
JinK001 <kyj9447@gmail.com>
2025-12-10 06:36:45 +0000

    항공사 아이콘 serve 경로 변경

--- src/main/java/renewal/awesome_travel/config/WebConfig.java
+++ src/main/java/renewal/awesome_travel/config/WebConfig.java
@@ -1,22 +1,9 @@
 package renewal.awesome_travel.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${iconLocation}")
-    private String iconDir;
-
-    @Override
-    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
-        // 항공사 아이콘 (공용)
-        String iconPath = "file:" + iconDir + "/";
-        registry.addResourceHandler("/icon/**")
-                .addResourceLocations(iconPath);
-    }
 }
